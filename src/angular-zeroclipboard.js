75c999df93a86e331491e608d556305bc713eefb
Stan Bondi <stan@fixate.it>
2014-07-30 12:41:46 +0200

    Change directive to work in a more angular way

--- src/angular-zeroclipboard.js
+++ src/angular-zeroclipboard.js
@@ -1,76 +1,65 @@
-angular.module('angular.zeroclipboard', []).
-provider('uiZeroclipConfig', function() {
+angular.module('zeroclipboard', [])
+  .provider('uiZeroclipConfig', function() {
     // default configs
     var _zeroclipConfig = {
-        buttonClass: '',
-        moviePath: "ZeroClipboard.swf",
-        trustedDomains: [window.location.host],
-        cacheBust: true,
-        forceHandCursor: false,
-        zIndex: 999999999,
-        debug: true,
-        title: null,
-        autoActivate: true,
-        flashLoadTimeout: 30000,
-        hoverClass: "zeroclipboard-is-hover",
-        activeClass: "zeroclipboard-is-active"
-    };
-    var _options = {
-        buttonClass: '',
-        buttonText: 'Copy',
-        emitEvent: false
+      buttonClass: '',
+      moviePath: "ZeroClipboard.swf",
+      trustedDomains: [window.location.host],
+      cacheBust: true,
+      forceHandCursor: false,
+      zIndex: 999999999,
+      debug: false,
+      title: null,
+      autoActivate: true,
+      flashLoadTimeout: 30000,
+      hoverClass: "zeroclipboard-is-hover",
+      activeClass: "zeroclipboard-is-active"
     };
     this.setZcConf = function(zcConf) {
-        angular.extend(_zeroclipConfig, zcConf);
-    };
-    this.setOptions = function(options) {
-        angular.extend(_options, options);
+      angular.extend(_zeroclipConfig, zcConf);
     };
     this.$get = function() {
-        return {
-            zeroclipConfig: _zeroclipConfig,
-            options: _options
-        }
+      return {
+        zeroclipConfig: _zeroclipConfig
+      }
     };
-}).
-directive('uiZeroclip', ['$document', '$window', 'uiZeroclipConfig',
-    function($document, $window, uiZeroclipConfig) {
-        var zeroclipConfig = uiZeroclipConfig.zeroclipConfig || {};
-        var options = uiZeroclipConfig.options;
-        var _id = 0;
+  })
 
-        function insertAfter(newNode, referenceNode) {
-            referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-        }
+  .directive('uiZeroclip', ['$document', '$window', 'uiZeroclipConfig',
+      function($document, $window, uiZeroclipConfig) {
+
+        var zeroclipConfig = uiZeroclipConfig.zeroclipConfig || {};
+        var ZeroClipboard = $window.ZeroClipboard
+
         return {
-            priority: 10,
-            link: function(scope, elm, attrs) {
-                // config
-                ZeroClipboard.config(zeroclipConfig);
-                var btn = elm[0];
-                if (!attrs.id) {
-                    attrs.$set('id', 'uiZeroclip' + _id);
-                    btn = document.createElement('button');
-                    btn.appendChild(document.createTextNode(options.buttonText));
-                    btn.setAttribute('data-clipboard-target', 'uiZeroclip' + _id);
-                    btn.setAttribute('class', options.buttonClass);
-                    _id++;
-                    insertAfter(btn, elm[0]);
-                }
-                if (angular.isFunction(ZeroClipboard)) {
-                    scope.client = new ZeroClipboard(btn);
-                }
-                var _events = ['load', 'mouseover', 'mouseout', 'mouseup', 'mousedown', 'complete', 'dataRequested', 'noflash', 'wrongflash'];
-                _events.forEach(function(evt) {
-                    if (options.emitEvent) {
-                        scope.client.on(evt, function() {
-                            scope.$emit('ZeroClipboard.' + evt);
-                        });
-                    } else {
-                        scope.client.on(evt, options[evt]);
-                    }
-                })
+          scope: {
+            onCopied: '&zeroclipCopied',
+            client: '=?uiZeroclip',
+            value: '=zeroclipModel'
+          },
+          link: function(scope, element, attrs) {
+            // config
+            ZeroClipboard.config(zeroclipConfig);
+            var btn = element[0];
+
+            if (angular.isFunction(ZeroClipboard)) {
+              client = scope.client = new ZeroClipboard(btn);
             }
+
+            scope.$watch('value', function(v) {
+              element.attr('data-clipboard-text', v);
+            });
+
+            client.on('complete', function _completeHnd(e) {
+              scope.$apply(function() {
+                scope.onCopied({$event: e});
+              });
+            });
+
+            scope.$on('$destroy', function() {
+              client.off('complete', _completeHnd)
+            });
+          }
         }
-    }
-]);
+      }
+  ]);
